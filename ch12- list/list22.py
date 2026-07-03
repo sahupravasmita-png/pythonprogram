4a391158b006259ecd5270442ca5e5f850608092
PRAVA DEVLOPER <sahupravasmita@gmail.com>
2026-07-03 18:07:22 +0530

    day23

---
+++ ch12- list/list22.py
@@ -0,0 +1,10 @@
+L=[0,0,0,0]
+s=0
+print("enter",len(L),"elements")
+for i in range(len(L)):
+	print("enter element",i+1)
+	L[i]=int(input())
+	s=s+L[i]
+	avg=s/4
+print("total=",s)
+print("average=",avg)
