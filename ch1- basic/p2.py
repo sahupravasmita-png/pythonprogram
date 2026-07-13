c27e56e8bc6803315aaa22666878d626a8c05154
PRAVA DEVLOPER <sahupravasmita@gmail.com>
2026-07-13 22:47:16 +0530

    day33

---
+++ ch1- basic/p2.py
@@ -0,0 +1,8 @@
+print("enter a number ")
+no1=int(input())
+print("enter another number ")
+no2=int(input())
+s=no1+no2
+print("first no=",no1)
+print("second no=",no2)
+print("sum =",s)
---
+++ ch15-dictionary/p.py
@@ -0,0 +1,3 @@
+d={}
+d=d.fromkeys("prava")    #formkeys-make only keys and values shows none & order will maintain
+print(d)
