fd2fee4748b4c4b08fca84c80a6ad72597941c4d
PRAVA DEVLOPER <sahupravasmita@gmail.com>
2026-07-15 14:41:51 +0530

    day34

---
+++ ch17-NumPy/matrix add.py
@@ -0,0 +1,12 @@
+import numpy as np
+# Create two matrices
+A = np.array([[1, 2, 3],[4, 5, 6]])
+B = np.array([[7, 8, 9],[10, 11, 12]])
+# Matrix addition
+C = A + B
+print("Matrix A:")
+print(A)
+print("\nMatrix B:")
+print(B)
+print("\nSum of matrices:")
+print(C)
---
+++ ch17-NumPy/matrix mult.py
@@ -0,0 +1,5 @@
+import numpy as np
+a=np.array([[7,9],[2,4]])
+b=np.array([[5,7],[9,3]])
+c=np.multiply(a,b)
+print(c)
