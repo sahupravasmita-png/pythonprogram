0e88d67787568f8d3c14dabcb1c0dfd96d90f776
PRAVA DEVLOPER <sahupravasmita@gmail.com>
2026-07-17 22:15:33 +0530

    day37

---
+++ ch3- if else/p2.py
@@ -0,0 +1,13 @@
+no1=int(input("enter 1st no"))
+no2=int(input("enter 2nd no"))
+no3=int(input("enter 3rd no"))
+if no1>=no2:
+	if no1>=no3:
+		print("first no is bigger")
+	else:
+		print("third no is bigger")
+else:
+	if no2>=no3:
+	    print("second no is bigger")
+	else:
+		print("third no is bigger")
